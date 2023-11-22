900d67f0bd39c847ce2125c9d2bc611387c8aa6a
pn1137 <pnaugle@usf.edu>
2023-11-21 23:15:02 -0500

    Updated Battle.cs and CharacterStats.cs as well as to allow for health during battle to be shown visually

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
+using UnityEngine.UI;
 
 public class Battle : MonoBehaviour
 {
-    static public GameObject character;
-    private GameObject enemy = null;
+    static public GameObject playerModel;
+    static private CharacterStats player;
+    private GameObject enemyModel = null;
+    private CharacterStats enemy;
     static private bool isFighting = false;
     static private GameObject currentEnemy = null;
+    static private Slider playerHealthBar;
+    static private Slider enemyHealthBar;
     private float imageTargetDimensionX;
     private float imageTargetDimensionY;
+    private float lastAttackTime = 0f;
+    private bool isPlayersTurnToAttack = true;
 
 #pragma warning disable CS0618 // Type or member is obsolete
     private VirtualButtonBehaviour buttonBehaviour;
@@ -21,25 +28,30 @@ public class Battle : MonoBehaviour
         buttonBehaviour = GetComponentInChildren<VirtualButtonBehaviour>();
         buttonBehaviour.RegisterOnButtonPressed(OnButtonPressed);
 
+        playerHealthBar = GameObject.Find("Player Health Bar").GetComponentInChildren<Slider>();
+        enemyHealthBar = GameObject.Find("Enemy Health Bar").GetComponentInChildren<Slider>();
+
         Vector2 imageDimensions = this.GetComponent<ImageTargetBehaviour>().GetSize();
         imageTargetDimensionX = imageDimensions.x;
         imageTargetDimensionY = imageDimensions.y;
-        Debug.Log("--Dimensions: " + imageTargetDimensionX + ", " + imageTargetDimensionY);
+
+        enemy = GetComponent<CharacterStats>();
 
         foreach (Transform child in this.transform)
         {
             if (child.CompareTag("Enemy"))
             {
-                enemy = child.gameObject;
+                enemyModel = child.gameObject;
                 break;
             }
         }
 
-        if (character == null)
+        if (playerModel == null)
         {
-            character = GameObject.Instantiate(GameObject.Find("CharacterSelectMenu").GetComponent<CharacterSelect>().selectedCharacter); // Duplicate character
-            character.active = false;
-            character.layer = LayerMask.NameToLayer("Default"); // Set layer to Default (instead of UI)
+            player = GetComponent<CharacterStats>();
+            playerModel = GameObject.Instantiate(GameObject.Find("CharacterSelectMenu").GetComponent<CharacterSelect>().selectedCharacter); // Duplicate character
+            playerModel.active = false;
+            playerModel.layer = LayerMask.NameToLayer("Default"); // Set layer to Default (instead of UI)
         }
     }
 
@@ -47,7 +59,7 @@ public class Battle : MonoBehaviour
     {
         Debug.Log("--OnButtonPressed: " + vb.VirtualButtonName);
 
-        Debug.Log("--Character: " + character.name);
+        Debug.Log("--Character: " + playerModel.name);
 
         isFighting = !isFighting;
 
@@ -59,16 +71,16 @@ public class Battle : MonoBehaviour
             {
                 // Enemy fighting setup
                 currentEnemy = this.gameObject;
-                enemy.transform.position = new Vector3(enemy.transform.position.x - imageTargetDimensionX / 2, enemy.transform.position.y, enemy.transform.position.z);
-                enemy.transform.Rotate(0, -90, 0);
+                enemyModel.transform.position = new Vector3(enemyModel.transform.position.x - imageTargetDimensionX / 2, enemyModel.transform.position.y, enemyModel.transform.position.z);
+                enemyModel.transform.Rotate(0, -90, 0);
 
-                // Character fighting setup
-                character.transform.parent = this.transform;
-                character.transform.position = this.transform.position;
-                character.transform.localScale = new Vector3(.025f, .025f, .025f);
-                character.transform.position += new Vector3(imageTargetDimensionX / 2, 0, 0);
-                character.transform.LookAt(enemy.transform);
-                character.active = true;
+                // Player fighting setup
+                playerModel.transform.parent = this.transform;
+                playerModel.transform.position = this.transform.position;
+                playerModel.transform.localScale = new Vector3(.025f, .025f, .025f);
+                playerModel.transform.position += new Vector3(imageTargetDimensionX / 2, 0, 0);
+                playerModel.transform.LookAt(enemyModel.transform);
+                playerModel.active = true;
             }
         }
         else
@@ -77,9 +89,9 @@ public class Battle : MonoBehaviour
             {
                 // Restore
                 currentEnemy = null;
-                enemy.transform.position = new Vector3(enemy.transform.position.x + imageTargetDimensionX / 2, enemy.transform.position.y, enemy.transform.position.z);
-                enemy.transform.Rotate(0, 90, 0);
-                character.active = false;
+                enemyModel.transform.position = new Vector3(enemyModel.transform.position.x + imageTargetDimensionX / 2, enemyModel.transform.position.y, enemyModel.transform.position.z);
+                enemyModel.transform.Rotate(0, 90, 0);
+                playerModel.active = false;
             }
         }
     }
@@ -87,11 +99,32 @@ public class Battle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (isFighting) {
-        //Time t = now
-        // if (t == t+.5 seconds) {
-        //  enemyAttack
-        //}
-        //}
+        if (isFighting && currentEnemy == this.gameObject) {
+            float time = Time.time;
+            
+            if (time > lastAttackTime + .5f)
+            {
+                Debug.Log("Time: " + time.ToString());
+                Debug.Log("Last: " + lastAttackTime.ToString());
+                lastAttackTime = time;
+
+                if (isPlayersTurnToAttack)
+                {
+                    player.attackCharacter(enemy);
+                    Debug.Log("Enemy Health: " + enemy.getHealth().ToString());
+                    enemyHealthBar.maxValue = enemy.getMaxHealth();
+                    enemyHealthBar.value = enemy.getHealth();
+                }
+                else
+                {
+                    enemy.attackCharacter(player);
+                    Debug.Log("Player Health: " + player.getHealth().ToString());
+                    playerHealthBar.maxValue = player.getMaxHealth();
+                    playerHealthBar.value = player.getHealth();
+                }                    
+                
+                isPlayersTurnToAttack = !isPlayersTurnToAttack;
+            }            
+        }
     }
 }
--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia.ARFoundation;
 
 public class CharacterStats : MonoBehaviour
 {
@@ -8,21 +9,45 @@ public class CharacterStats : MonoBehaviour
     public float attack = 5.0f; // Amount of possible damage to health of target character
     public float defense = 3.0f; // Amount of reduction from damage to health
     private float health; // Current health of a character
-    private CharacterStats targetCharacter;
+    static private GameObject win;
+    static private GameObject lose;
 
     void Awake()
     {
         health = maxHealth;
+
+        if (win == null)
+        {
+            win = GameObject.Find("WinMenu");
+            Debug.Log("Win " + win == null ? "Found" : "Not Found");
+            win.SetActive(false);
+            lose = GameObject.Find("LoseMenu");
+            Debug.Log("Lose " + lose == null ? "Found" : "Not Found");
+            lose.SetActive(false);
+        }
     }
 
     public void removeHealth(float amount)
     {
-        health -= amount;
+        if (amount > 0)
+        {
+            health -= amount;
+        }
 
         if (health <= 0.0f)
         {
             // destroy
             // trigger something
+            if (this.CompareTag("Player"))
+            {
+                //lose.active = true;
+                win.active = true;
+            }
+            else if (this.CompareTag("Enemy"))
+            {
+                //win.active = true;
+                lose.active = true;
+            }
         }
     }
 
@@ -36,16 +61,21 @@ public class CharacterStats : MonoBehaviour
         }
     }
 
-    public void attackCharacter()
+    public float getHealth()
+    {
+        return health;
+    }
+
+    public float getMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public void attackCharacter(CharacterStats targetCharacter)
     {
         if (targetCharacter != null) 
         {
-            targetCharacter.removeHealth(attack - targetCharacter.defense);
+            targetCharacter.removeHealth(attack * (1/targetCharacter.defense));
         }
-    }
-
-    public void setTargetCharacter(GameObject target)
-    {
-        targetCharacter = target.GetComponent<CharacterStats>();
     }
 }
